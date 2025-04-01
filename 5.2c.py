576b90786ab8d6bf35efe167acc5adb2b6942815
uppalkrish <104903490+uppalkrish@users.noreply.github.com>
2025-04-02 09:19:04 +1100

    Add files via upload

---
+++ 5.2c.py
@@ -0,0 +1,63 @@
+import sys
+import RPi.GPIO as GPIO
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QSlider, QPushButton
+from PyQt6.QtCore import Qt
+
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+LED1, LED2, LED3 = 17, 18, 27
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
+GPIO.setup(LED3, GPIO.OUT)
+
+# Set up PWM for LEDs
+pwm1 = GPIO.PWM(LED1, 1000)
+pwm2 = GPIO.PWM(LED2, 1000)
+pwm3 = GPIO.PWM(LED3, 1000)
+pwm1.start(0)
+pwm2.start(0)
+pwm3.start(0)
+
+class LEDApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("LED Brightness Control")
+        layout = QVBoxLayout()
+
+        self.slider1 = QSlider(Qt.Orientation.Horizontal)
+        self.slider2 = QSlider(Qt.Orientation.Horizontal)
+        self.slider3 = QSlider(Qt.Orientation.Horizontal)
+
+        self.slider1.setRange(0, 100)
+        self.slider2.setRange(0, 100)
+        self.slider3.setRange(0, 100)
+
+        self.slider1.valueChanged.connect(lambda value: self.change_brightness(pwm1, value))
+        self.slider2.valueChanged.connect(lambda value: self.change_brightness(pwm2, value))
+        self.slider3.valueChanged.connect(lambda value: self.change_brightness(pwm3, value))
+
+        layout.addWidget(self.slider1)
+        layout.addWidget(self.slider2)
+        layout.addWidget(self.slider3)
+        
+        exit_btn = QPushButton("Exit")
+        exit_btn.clicked.connect(self.close_app)
+        layout.addWidget(exit_btn)
+        
+        self.setLayout(layout)
+    
+    def change_brightness(self, pwm, value):
+        pwm.ChangeDutyCycle(value)
+    
+    def close_app(self):
+        pwm1.stop()
+        pwm2.stop()
+        pwm3.stop()
+        GPIO.cleanup()
+        self.close()
+
+app = QApplication(sys.argv)
+window = LEDApp()
+window.show()
+sys.exit(app.exec())
+
