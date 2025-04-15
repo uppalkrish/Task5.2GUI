2ca4991d749b6f9c2fb319f816d9135bde60d344
uppalkrish <104903490+uppalkrish@users.noreply.github.com>
2025-04-16 09:03:23 +1000

    Update 5.2c.py

--- 5.2c.py
+++ 5.2c.py
@@ -1,7 +1,8 @@
 import sys
 import RPi.GPIO as GPIO
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QSlider, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QSlider, QPushButton, QLabel
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPalette, QColor
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
@@ -24,31 +25,48 @@ class LEDApp(QWidget):
         self.setWindowTitle("LED Brightness Control")
         layout = QVBoxLayout()
 
+        # Red LED
+        self.label1 = QLabel("Red LED")
+        self.set_label_color(self.label1, "red")
         self.slider1 = QSlider(Qt.Orientation.Horizontal)
-        self.slider2 = QSlider(Qt.Orientation.Horizontal)
-        self.slider3 = QSlider(Qt.Orientation.Horizontal)
-
         self.slider1.setRange(0, 100)
-        self.slider2.setRange(0, 100)
-        self.slider3.setRange(0, 100)
-
         self.slider1.valueChanged.connect(lambda value: self.change_brightness(pwm1, value))
-        self.slider2.valueChanged.connect(lambda value: self.change_brightness(pwm2, value))
-        self.slider3.valueChanged.connect(lambda value: self.change_brightness(pwm3, value))
-
+        layout.addWidget(self.label1)
         layout.addWidget(self.slider1)
+
+        # Green LED
+        self.label2 = QLabel("Green LED")
+        self.set_label_color(self.label2, "green")
+        self.slider2 = QSlider(Qt.Orientation.Horizontal)
+        self.slider2.setRange(0, 100)
+        self.slider2.valueChanged.connect(lambda value: self.change_brightness(pwm2, value))
+        layout.addWidget(self.label2)
         layout.addWidget(self.slider2)
+
+        # Blue LED
+        self.label3 = QLabel("Blue LED")
+        self.set_label_color(self.label3, "blue")
+        self.slider3 = QSlider(Qt.Orientation.Horizontal)
+        self.slider3.setRange(0, 100)
+        self.slider3.valueChanged.connect(lambda value: self.change_brightness(pwm3, value))
+        layout.addWidget(self.label3)
         layout.addWidget(self.slider3)
-        
+
+        # Exit button
         exit_btn = QPushButton("Exit")
         exit_btn.clicked.connect(self.close_app)
         layout.addWidget(exit_btn)
-        
+
         self.setLayout(layout)
-    
+
+    def set_label_color(self, label, color):
+        palette = label.palette()
+        palette.setColor(QPalette.ColorRole.WindowText, QColor(color))
+        label.setPalette(palette)
+
     def change_brightness(self, pwm, value):
         pwm.ChangeDutyCycle(value)
-    
+
     def close_app(self):
         pwm1.stop()
         pwm2.stop()
@@ -60,4 +78,3 @@ app = QApplication(sys.argv)
 window = LEDApp()
 window.show()
 sys.exit(app.exec())
-
